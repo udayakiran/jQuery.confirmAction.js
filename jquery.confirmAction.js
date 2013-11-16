a24f7bc25d707b03de45ab9eabb6dfa1075fb318
Udaya Kiran <udaykiran.vit@gmail.com>
2013-11-16 20:09:52 +0530

    Adding a condition based on which the confirmation box is shown or not

--- jquery.confirmAction.js
+++ jquery.confirmAction.js
@@ -6,13 +6,13 @@
  * @author Uday kiran
  * @author Uday kiran<udaykiran.vit@gmail.com>
  *
- * @url <to be updated>
+ * @url <https://github.com/udayakiran/jQuery.confirmAction.js>
  */
 (function($) {
 
   /**
    * Confirm Action on any item click.
-   * @param options {text,  container, show, confirm, cancel, confirmButton, cancelButton}
+   * @param options {text,  container, show, confirm, cancel, confirmbyFunction, cancelOnTrue, confirmButton, cancelButton}
    */
   $.fn.confirmAction = function(options) {
     if (typeof options === 'undefined') {
@@ -22,13 +22,8 @@
     options.button = $(this);
 
     this.click(function(e) {
-      if (options.confimationNeeded !== undefined && options.confimationNeeded() === false) {
-        //do nothing. let the original onclick get fired if one exists.
-      } else {
         e.preventDefault();
-
         $.confirmAction(options, e);
-      }
     });
 
     return this;
@@ -43,9 +38,11 @@
     var settings = $.extend({
       text: "Are you sure?",
       confirmButton: "Yes",
-      cancelButton: "Cancel",
-      post: false,
+      cancelButton: "No",
       container: null,
+      isConfirmationNeeded: function() {
+        return true;
+      },
       show: function(o) {
         //just do nothing
       },
@@ -59,10 +56,10 @@
     }, options);
 
     var buttons = '<a class="confirm-link">' + settings.confirmButton + '</a>'
-            + '<span class="confirm-link-seperator"> | </span>'
+            + '<span class="confirm-link-seperator" style="padding:0px 3px;color:gray"> l </span>'
             + '<a class="cancel-link">' + settings.cancelButton + '</a>';
     var containerHTML = '<div class="confirmation-message">'
-            + '<span class="confirmation-text">' + settings.text + '</span>'
+            + '<span class="confirmation-text" style="padding-right:3px">' + settings.text + '</span>'
             + '<span class="confirmation-links">' + buttons + '</span>'
             + '</div>';
 
@@ -75,8 +72,12 @@
       settings.cancel(settings.button);
     });
 
-    settings.container.show();
-    settings.show(settings.button);
+    if (settings.isConfirmationNeeded()) {
+      settings.container.show();
+      settings.show(settings.button);
+    } else {
+      settings.confirm(settings.button);
+    }
   }
 
 })(jQuery);
